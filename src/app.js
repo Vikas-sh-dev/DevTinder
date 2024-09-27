c47081a16c3b4892f52843fcd230a87369c52a32
Vikas Bhardwaj <vikas.bhardwaj@grazitti.local>
2024-09-28 01:45:02 +0530

    set express and nodemon

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+
+app.use("/about", (req, res) => {
+    res.send("This is about page");
+});
+
+app.use((req, res) => {
+    res.send("Hello from the server");
+});
+
+app.listen(3000, () => {
+    console.log("server started successfully");
+});
