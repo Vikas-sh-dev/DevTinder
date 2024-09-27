39b444118815057f3c83e0a41169ec399329f8ff
Vikas Bhardwaj <vikas.bhardwaj@grazitti.local>
2024-09-28 03:14:46 +0530

    playing with routes

--- src/app.js
+++ src/app.js
@@ -2,17 +2,27 @@ const express = require("express");
 
 const app = express();
 
-app.use("/", (req, res) => {
-    res.send("just / thing");
-});
 
-app.use("/about", (req, res) => {
+
+app.get("/about/:userId", (req, res) => {
+    console.log(req.params);
     res.send("This is about page");
 });
 
-app.use((req, res) => {
+app.post("/about", (req, res) => {
+    res.send("Posted");
+});
+
+app.delete("/about", (req, res) => {
+    res.send("Deleted");
+});
+
+
+app.get((req, res) => {
     res.send("Hello from the server");
 });
+
+
 
 app.listen(3000, () => {
     console.log("server started successfully");
